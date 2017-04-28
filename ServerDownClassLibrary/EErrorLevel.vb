66e04304f8e97b4c646c2202b95921d3f3dc5863
ddfi2014 <DoschDavid@dv-schulen.de>
2017-04-28 11:39:57 +0200

    Add files via upload

---
+++ ServerDownClassLibrary/EErrorLevel.vb
@@ -0,0 +1,6 @@
+﻿Public Enum EErrorLevel
+    None = -1
+    Hint = 10
+    Warning = 20
+    CriticalError = 30
+End Enum
--- ServerDownClassLibrary/IView.vb
+++ ServerDownClassLibrary/IView.vb
@@ -1,4 +1,6 @@
 ﻿Public Interface IView
     Sub AddLogItem(item As String)
     Sub ClearList()
+    'Sub ToggleLogging()
+    Sub ShowErrorMessage(message As String, Optional errorlevel As EErrorLevel = EErrorLevel.None)
 End Interface
